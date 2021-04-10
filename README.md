d1acd9c4a111f14ba661be5f2c937d6e270cf708
Saxon Hunt <shunt@twilio.com>
2021-04-10 00:18:56 -0700

    update readme, edit file in user's editor and save to .garden

---
+++ README.md
@@ -0,0 +1,24 @@
+# Digital Garden
+
+A CLI tool for the creation and maintenance of Digital Gardens.
+
+## Commands
+
+### Setting the garden path
+
+```shell
+GARDEN_PATH=~/github/my-digital-garden garden write
+garden -p ~/github/my-digital-garden write
+garden --garden_path ~/github/my-digital-garden write
+```
+
+### write
+
+Open a new file to write in our digital garden. Since we don't necessarily konw what we want
+to title what we're writing, we'll leave the title as optional and ask the user for it later
+if they don't provide it up front.
+
+```shell
+garden write
+garden write -t "Some Title"
+```
--- src/write.rs
+++ src/write.rs
@@ -1,8 +1,42 @@
+use color_eyre::{eyre::WrapErr, Result};
+use edit::edit_file;
+use std::io::{Read, Seek, SeekFrom, Write};
 use std::path::PathBuf;
 
-use color_eyre::Result;
+const TEMPLATE: &[u8; 2] = b"# ";
 
 pub fn write(garden_path: PathBuf, title: Option<String>) -> Result<()> {
-    dbg!(garden_path, title);
+    dbg!(&garden_path);
+    // Open temporary file in preferred text editor
+    let (mut file, filepath) = edit::Builder::new()
+        .suffix(".md")
+        .rand_bytes(5)
+        .tempfile_in(&garden_path)
+        .wrap_err("Failed to create temp WIP file")?
+        .keep()
+        .wrap_err("Failed to keep tempfile")?;
+
+    file.write_all(TEMPLATE)?;
+
+    // let the user write whatever they want in their favorite editor
+    // before returning to the cli and finishing up
+    edit_file(filepath)?;
+
+    //Read the user's changes back from the file into a string
+    let mut contents = String::new();
+    file.seek(SeekFrom::Start(0))?;
+    file.read_to_string(&mut contents)?;
+
+    // use `title if the user passed it in,
+    // otherwise try to find a heading in the markdown
+    let document_title = title.or_else(|| {
+        contents
+            .lines()
+            // find a markdown Heading, removing the .md header syntax
+            .find(|v| v.starts_with("# "))
+            .map(|maybe_line| maybe_line.trim_start_matches("# ").to_string())
+    });
+
+    dbg!(contents, document_title);
     todo!()
 }
